997b06b6406bf94ceeabe464e27324772c02d770
Patrick McDonnell <pmcdonnell@findmypast.com>
2017-04-03 10:26:13 +0100

    Shell of project. Paired: Steven B

---
+++ lib/speechmatex.ex
@@ -0,0 +1,18 @@
+defmodule Speechmatex do
+  @moduledoc """
+  Documentation for Speechmatex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Speechmatex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/speechmatex_test.exs
@@ -0,0 +1,8 @@
+defmodule SpeechmatexTest do
+  use ExUnit.Case
+  doctest Speechmatex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
